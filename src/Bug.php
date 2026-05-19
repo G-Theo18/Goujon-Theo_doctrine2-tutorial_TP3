231f6a802f996dca496d80b07142723418abb289
tg853309 <tgou58@gmail.com>
2026-05-19 08:15:25 +0200

    Ajout des entités Bug et User (partie 2)

--- src/Bug.php
+++ src/Bug.php
@@ -1,39 +1,55 @@
 <?php
 // src/Bug.php
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+
 #[ORM\Entity]
 #[ORM\Table(name: 'bugs')]
 class Bug
 {
-#[ORM\Id]
-#[ORM\Column(type: 'integer')]
-#[ORM\GeneratedValue]
-private int|null $id;
-#[ORM\Column(type: 'string')]
-private string $description;
-#[ORM\Column(type: 'datetime')]
-private DateTime $created;
-#[ORM\Column(type: 'string')]
-private string $status;
-public function getId(): int|null {
-return $this->id;
-}
-public function getDescription(): string {
-return $this->description;
-}
-public function setDescription(string $description): void {
-$this->description = $description;
-}
-public function setCreated(DateTime $created) {
-$this->created = $created;
-}
-public function getCreated(): DateTime {
-return $this->created;
-}
-public function setStatus($status): void {
-$this->status = $status;
-}
-public function getStatus():string {
-return $this->status;
-}
+    #[ORM\Id]
+    #[ORM\Column(type: 'integer')]
+    #[ORM\GeneratedValue]
+    private int|null $id;
+    #[ORM\Column(type: 'string')]
+    private string $description;
+    #[ORM\Column(type: 'datetime')]
+    private DateTime $created;
+    #[ORM\Column(type: 'string')]
+    private string $status;
+    public function getId(): int|null
+    {
+        return $this->id;
+    }
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+    public function setDescription(string $description): void
+    {
+        $this->description = $description;
+    }
+    public function setCreated(DateTime $created)
+    {
+        $this->created = $created;
+    }
+    public function getCreated(): DateTime
+    {
+        return $this->created;
+    }
+    public function setStatus($status): void
+    {
+        $this->status = $status;
+    }
+    public function getStatus(): string
+    {
+        return $this->status;
+    }
+
+    private $products;
+    public function __construct()
+    {
+        $this->products = new ArrayCollection();
+    }
 }
--- src/User.php
+++ src/User.php
@@ -1,28 +1,39 @@
 <?php
 // src/User.php
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+
 #[ORM\Entity]
 #[ORM\Table(name: 'users')]
 class User
 {
-/** @var int */
-#[ORM\Id]
-#[ORM\GeneratedValue]
-#[ORM\Column(type: 'integer')]
-private int|null $id = null;
-/** @var string */
-#[ORM\Column(type: 'string')]
-private string $name;
-public function getId(): int|null
-{
-return $this->id;
-}
-public function getName(): string
-{
-return $this->name;
-}
-public function setName(string $name): void
-{
-$this->name = $name;
-}
+    /** @var int */
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private int|null $id = null;
+    /** @var string */
+    #[ORM\Column(type: 'string')]
+    private string $name;
+    public function getId(): int|null
+    {
+        return $this->id;
+    }
+    public function getName(): string
+    {
+        return $this->name;
+    }
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    private $reportedBugs = null;
+    private $assignedBugs = null;
+    public function __construct()
+    {
+        $this->reportedBugs = new ArrayCollection();
+        $this->assignedBugs = new ArrayCollection();
+    }
+
 }
