6f76f6181ced49acb220a228ee75cd988b88e2ae
tg853309 <tgou58@gmail.com>
2026-05-19 08:32:14 +0200

    Ajout des entités Bug et User (partie 4)

--- src/Bug.php
+++ src/Bug.php
@@ -1,5 +1,6 @@
 <?php
 // src/Bug.php
+use DateTime;
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
@@ -12,75 +13,95 @@ class Bug
     #[ORM\Column(type: 'integer')]
     #[ORM\GeneratedValue]
     private int|null $id;
+
     #[ORM\Column(type: 'string')]
     private string $description;
+
     #[ORM\Column(type: 'datetime')]
     private DateTime $created;
+
     #[ORM\Column(type: 'string')]
     private string $status;
+
+    #[ORM\ManyToOne(targetEntity: User::class, inversedBy: 'assignedBugs')]
+    private ?User $engineer = null;
+
+    #[ORM\ManyToOne(targetEntity: User::class, inversedBy: 'reportedBugs')]
+    private ?User $reporter = null;
+
+    #[ORM\ManyToMany(targetEntity: Product::class)]
+    private Collection $products;
+
+    public function __construct()
+    {
+        $this->products = new ArrayCollection();
+        $this->created = new DateTime();
+    }
+
     public function getId(): int|null
     {
         return $this->id;
     }
+
     public function getDescription(): string
     {
         return $this->description;
     }
+
     public function setDescription(string $description): void
     {
         $this->description = $description;
     }
-    public function setCreated(DateTime $created)
-    {
-        $this->created = $created;
-    }
+
     public function getCreated(): DateTime
     {
         return $this->created;
     }
-    public function setStatus($status): void
+
+    public function setCreated(DateTime $created): void
     {
-        $this->status = $status;
+        $this->created = $created;
     }
+
     public function getStatus(): string
     {
         return $this->status;
     }
 
-    private $products;
-    public function __construct()
+    public function setStatus(string $status): void
     {
-        $this->products = new ArrayCollection();
+        $this->status = $status;
     }
 
-    private User $engineer;
-    private User $reporter;
+    public function getEngineer(): User
+    {
+        return $this->engineer;
+    }
+
     public function setEngineer(User $engineer): void
     {
         $engineer->assignedToBug($this);
         $this->engineer = $engineer;
     }
+
+    public function getReporter(): User
+    {
+        return $this->reporter;
+    }
+
     public function setReporter(User $reporter): void
     {
         $reporter->addReportedBug($this);
         $this->reporter = $reporter;
-    }
-    public function getEngineer(): User
-    {
-        return $this->engineer;
-    }
-    public function getReporter(): User
-    {
-        return $this->reporter;
     }
 
     public function assignToProduct(Product $product): void
     {
         $this->products[] = $product;
     }
+
     public function getProducts()
     {
         return $this->products;
     }
-
 }
--- src/User.php
+++ src/User.php
@@ -2,12 +2,12 @@
 // src/User.php
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 
 #[ORM\Entity]
 #[ORM\Table(name: 'users')]
 class User
 {
-    /** @var int */
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column(type: 'integer')]
@@ -15,31 +15,39 @@ class User
     /** @var string */
     #[ORM\Column(type: 'string')]
     private string $name;
-    public function getId(): int|null
-    {
-        return $this->id;
-    }
-    public function getName(): string
-    {
-        return $this->name;
-    }
-    public function setName(string $name): void
-    {
-        $this->name = $name;
-    }
 
-    private $reportedBugs = null;
-    private $assignedBugs = null;
+    #[ORM\OneToMany(targetEntity: Bug::class, mappedBy: 'reporter')]
+    private Collection $reportedBugs;
+
+    #[ORM\OneToMany(targetEntity: Bug::class, mappedBy: 'engineer')]
+    private Collection $assignedBugs;
+
     public function __construct()
     {
         $this->reportedBugs = new ArrayCollection();
         $this->assignedBugs = new ArrayCollection();
     }
 
+    public function getId(): int|null
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
     public function addReportedBug(Bug $bug): void
     {
         $this->reportedBugs[] = $bug;
     }
+
     public function assignedToBug(Bug $bug): void
     {
         $this->assignedBugs[] = $bug;
